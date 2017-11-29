8b47d766d8f4fd9bc69dbf8d926084adec52be32
Kenichi Sakabe <saken649@gmail.com>
2017-11-29 21:56:01 +0900

    プロジェクト ファイルを追加します。

---
+++ LayoutTest/LayoutTest/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LayoutTest
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            btnHrefToAbsolute.Clicked += (sender, e) =>
+            {
+                this.Navigation.PushAsync(new AbsoluteLayoutPage());
+            };
+		}
+	}
+}
